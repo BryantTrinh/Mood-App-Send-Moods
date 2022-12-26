ed582f4556cda91a0d82020551b5cedbcb1d6158
vicearus <ajvicenc@uci.edu>
2022-12-26 12:32:43 -0800

    schemas created

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,30 @@
-DROP DATABASE IF EXISTS moods_db;
+DROP DATABASE IF EXISTS sendmoods_db;
+CREATE DATABASE sendmoods_db;
 
-CREATE DATABASE moods_db;
+USE sendmoods_db;
+CREATE TABLE users (
+user_id INT PRIMARY KEY,
+first_name TEXT(20),
+last_name TEXT(20),
+email VARCHAR(50),
+user_name TEXT(20),
+user_password VARCHAR(200)
+);
+
+CREATE TABLE posts (
+post_id INT PRIMARY KEY,
+post_body VARCHAR(999),
+post_title VARCHAR(100),
+post_date DATETIME,
+anger BOOL,
+confusion BOOL,
+fear BOOL,
+joy BOOL,
+sadness BOOL
+);
+
+CREATE TABLE sessions (
+session_id INT,
+user_id INT PRIMARY KEY,
+session_expires DATETIME
+);
