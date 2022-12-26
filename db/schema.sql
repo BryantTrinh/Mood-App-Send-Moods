ab8ecc2da9bc7597c8a5bfbe2bfd05afc4680bc7
vicearus <ajvicenc@uci.edu>
2022-12-26 14:08:50 -0800

    schema changes and server.js setup

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE sendmoods_db;
 
 USE sendmoods_db;
 CREATE TABLE users (
-user_id INT PRIMARY KEY,
+user_id INT AUTO_INCREMENT PRIMARY KEY,
 first_name TEXT(20),
 last_name TEXT(20),
 email VARCHAR(50),
@@ -12,10 +12,13 @@ user_password VARCHAR(200)
 );
 
 CREATE TABLE posts (
-post_id INT PRIMARY KEY,
+post_id INT AUTO_INCREMENT PRIMARY KEY,
 post_body VARCHAR(999),
 post_title VARCHAR(100),
 post_date DATETIME,
+post_day INT,
+post_month INT,
+post_year INT,
 anger BOOL,
 confusion BOOL,
 fear BOOL,
@@ -24,7 +27,7 @@ sadness BOOL
 );
 
 CREATE TABLE sessions (
-session_id INT,
-user_id INT PRIMARY KEY,
+session_id INT PRIMARY KEY,
+user_id INT,
 session_expires DATETIME
 );
