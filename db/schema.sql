b0df36981e8e4c639ee9796860a922607f16cdd2
Bryant Trinh <trinhbryant@gmail.com>
2023-01-01 19:57:33 -0800

    replaced schema from send_moods db to moods_db

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,27 @@
 DROP DATABASE IF EXISTS moods_db;
 
 CREATE DATABASE moods_db;
+USE moods_db;
+CREATE TABLE users (
+user_id INT AUTO_INCREMENT PRIMARY KEY,
+first_name TEXT(20),
+last_name TEXT(20),
+email VARCHAR(50),
+user_name TEXT(20),
+user_password VARCHAR(200)
+);
+
+CREATE TABLE posts (
+post_id INT AUTO_INCREMENT PRIMARY KEY,
+post_body VARCHAR(999),
+post_title VARCHAR(100),
+post_date DATETIME,
+post_day INT,
+post_month INT,
+post_year INT,
+anger BOOL,
+confusion BOOL,
+fear BOOL,
+joy BOOL,
+sadness BOOL
+);
