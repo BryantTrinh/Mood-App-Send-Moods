8d241e7697990a48ebe2eef3a216c3705cd8cc81
Nicole T <nbtrieu@uci.edu>
2023-01-02 14:40:31 +0700

    Complete layout for landing page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,88 +1,70 @@
-// Finished
-
 const router = require('express').Router();
-
 const { User } = require('../../models');
-
-// POST '/' , session.save, 
-// POST 'login', 
-// POST '/logout/
-
-// Creating username and password, passing through req.body. Middleware to be added in server.js
 
 router.post('/', async (req, res) => {
   try {
-    const newUser = await User.create
+    const newUser = await User.create({
       username: req.body.username,
       password: req.body.password,
-      first_name: req.body.firstname,
-      last_name: req.body.lastname,
-      email: req.body.email
-  });
-
-// req.session.save for  updating session data in the session store and have it persist across requests. Also requires middleware in server.js
+    });
 
     req.session.save(() => {
-      req.session.userId = newUser.id;
+      req.session.user_id = newUser.id; //
       req.session.username = newUser.username;
-      req.session.expires = newUser.expires;
       req.session.loggedIn = true;
+
+      res.status(200).json(newUser);
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { username: req.body.username } });
+
+    if (!user) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
     }
 
-    res.json(newUser);
-  });
+    const validPassword = await user.checkPassword(req.body.password);
 
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
 
-  // post for login, make async wait while it searches for the login username in database.
-
-  router.post('/login', async (req, res) => {
-    try {
-      const user = await User.findOne({
-        where: {
-          username: req.body.username,
-        },
-      });
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.username = user.username;
+      req.session.loggedIn = true;
       
-      if (!user) {
-        res.status(400).json({
-          message: 'Username was not found, please try again'});
-          return;
-        });
+      res.json({ user: user, message: 'You are now logged in!' });
+    });
 
-        const validPassword = user.CheckPassword(req.body.password);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
-        if (!validPassword) {
-          res.status(400).json({
-            message: 'You have entered in your password incorrectly'})
-            return;
-          }
-
-          // save session if everything is valid
-
-          req.session.save(() => {
-            req.session.userId = user.id;
-            req.session.username = user.username;
-            req.session.expires = newUser.expires;
-            req.session.loggedIn = true;
-
-            res.json({ user, message: 'You have logged in successfully!'});
-          });
-        } catch (err) {
-          res.status(400).json({ 
-            message: 'account is not found'
-          });
-
-
-// logout and add callback method req.session.destroy. 204 indicates (no content) and no response body. 404 indicates requested resource is not found on the server.
-
-router.post('/logout'. (req, res) => {
-  if(req.session.loggedIn) {
-    req.session.destory(() => {
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.json(`logged out`);
+      console.log('logged out');
       res.status(204).end();
     });
   } else {
+    res.json(`failed to log out`);
+    console.log(`failed to log out`);
     res.status(404).end();
   }
 });
 
-module.exports.router;
+module.exports = router;
