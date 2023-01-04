88a936a0a8b003249fb64333a6f09667ea59a603
Nicole T <nbtrieu@uci.edu>
2023-01-05 05:21:27 +0700

    Add username to GET all posts on profile

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
+const Emoji = require('./Emoji');
+const PostEmoji = require('./PostEmoji');
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
@@ -17,4 +19,14 @@ Comment.belongsTo(User, {
   onDelete: 'CASCADE'
 });
 
-module.exports = { User, Post, Comment };
+Post.belongsToMany(Emoji, {
+  through: PostEmoji,
+  foreignKey: 'post_id',
+});
+
+Emoji.belongsToMany(Post, {
+  through: PostEmoji,
+  foreignKey: 'emoji_id',
+})
+
+module.exports = { User, Post, Comment, Emoji };
