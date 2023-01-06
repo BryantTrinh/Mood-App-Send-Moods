e83ad60214b42700937d739dbc5dfd615302cf4e
vicearus <ajvicenc@uci.edu>
2023-01-06 13:07:21 -0800

    saved-post-routes added

---
+++ controllers/api/saved-post-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { SavedPost } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newSave = await SavedPost.create({
+            post_id: req.body.post_id,
+            user_id: req.body.user_id
+        });
+        res.status(200).json(newSave);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+module.exports = router;
