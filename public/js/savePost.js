3c615e3985ddd753343766c772e0bbc1c9efa5b0
vicearus <ajvicenc@uci.edu>
2023-01-06 15:59:13 -0800

    toggle for save button fixed

--- public/js/savePost.js
+++ public/js/savePost.js
@@ -1,32 +1,44 @@
 const savePostHandler = async (event) => {
-    event.preventDefault();
-    let saved
+  event.preventDefault()
+  
+  let selectedElement = event.target
+  let finder = event.target.id
+  let savePostIndex = finder.split(',')
+  let post_id = savePostIndex[0];
+  let user_id = savePostIndex[1];
 
-
-    let finder = event.target.id
-    let savePostIndex = finder.split(',')
-    let post_id = savePostIndex[0];
-    let user_id = savePostIndex[1];
-
-    console.log(savePostIndex[0])
-    console.log(savePostIndex[1])
-    if (!saved){
+  console.log(event.target)
+  console.log(savePostIndex[0])
+  console.log(savePostIndex[1])
+ 
+  if ((!selectedElement.getAttribute("saved")|| selectedElement.getAttribute("saved") == "false")) {
     fetch('/api/saved-posts', {
       method: 'POST',
       body: JSON.stringify({ post_id, user_id }),
       headers: { 'Content-type': 'application/json' }
     })
-  .then(
-    saved = true
-  )};
-    if (saved){
-      fetch('api/saved-posts/delete', {
-        method: 'POST',
-        body: JSON.stringify({post_id, user_id}),
-        headers: { 'Content-type': 'application/json' }
-      })
       .then(
-        saved = false
+        selectedElement.setAttribute("saved", "true"),
+        saved = selectedElement.getAttribute("saved"),
+        console.log(event.target),
+        console.log(saved),
+        selectedElement.setAttribute("src","assets/images/save-icon.png")
       )
-    }
+      return
+  };
+  if (saved = true) {
+    fetch('api/saved-posts/delete', {
+      method: 'POST',
+      body: JSON.stringify({ post_id, user_id }),
+      headers: { 'Content-type': 'application/json' }
+    })
+      .then(
+        selectedElement.setAttribute("saved", "false"),
+        saved = selectedElement.getAttribute("saved"),
+        console.log(event.target),
+        console.log(saved),
+        selectedElement.setAttribute("src","assets/images/dot-paper.png")
+      )
+      return
   }
+}
