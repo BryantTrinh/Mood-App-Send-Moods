317027bc459d5a954d3d8143f5832682a473664e
Bryant Trinh <trinhbryant@gmail.com>
2023-01-07 19:00:23 -0800

    commiting to pull

--- controllers/save-routes.js
+++ controllers/save-routes.js
@@ -1,10 +1,10 @@
 // covers everything in terms of rendering saved posts
 const router = require('express').Router();
-const { SavedPost, User } = require('../models');
+const { SavedPost, User, Emoji } = require('../models');
 const withAuth = require('../utils/auth');
 
 // GET all posts on profile
-router.get('/', withAuth, async (req, res) => {
+router.get('/saved', withAuth, async (req, res) => {
   // console.log('starting GET route for all posts on proi');
   try {
     const dbSavedPostData = await SavedPost.findAll({
@@ -19,14 +19,13 @@ router.get('/', withAuth, async (req, res) => {
           model: User,
           attributes: ['username'],
         },
-        }
       ]
     });
 
     const savedPosts = dbSavedPostData.map((savedPost) => savedPost.get({ plain: true }));
     // console.log(savedPosts);
 
-    res.render('profile-all-saved-posts', {
+    res.render('saved-posts', {
       layout: 'profile',
       savedPosts, 
     });
